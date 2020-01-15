aa1779574bafbd8f1b01eb00540e3639e379acdd
aaronjan <aaronjan98@gmail.com>
2020-01-15 12:31:03 -0800

    setted up the setValue fxn inside useLocalStorage hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,17 +2,13 @@ import { useState } from 'react';
 
 export const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
-    if (localStorage.getItem(key)) {
-      return JSON.parse(localStorage.getItem(key));
-    } else {
-      localStorage.setItem(key, JSON.stringify(initialValue));
-      return initialValue;
-    }
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
   });
 
   const setValue = newValue => {
     setStoredValue(newValue);
-    localStorage.setItem(key, JSON.stringify(newValue));
+    window.localStorage.setItem(key, JSON.stringify(newValue));
   };
 
   return [storedValue, setValue];
